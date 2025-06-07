6f5199bd84b6493a02242a6e5108fe67756cba1c
Mehmet Can KAHRİMAN <98193955+mehmetckahriman@users.noreply.github.com>
2025-06-07 19:10:26 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+# Her başlatmada daemon satırlarını sil
+sed -i '/daemon\s*off;/d;/daemon\s*on;/d' /etc/nginx/nginx.conf
 echo "NGINX CONF (daemon):"
 grep -n daemon /etc/nginx/nginx.conf || echo "YOK"
 
