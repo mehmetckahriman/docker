d48c94083412626e221ff213156ab2a8bc4e1f61
Mehmet Can KAHRİMAN <98193955+mehmetckahriman@users.noreply.github.com>
2025-06-07 19:05:18 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+echo "NGINX CONF (daemon):"
+grep -n daemon /etc/nginx/nginx.conf || echo "YOK"
+
 php bin/console doctrine:migrations:migrate --no-interaction --env=prod
 
 php-fpm81 -D
