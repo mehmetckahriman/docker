f88f7833611f667cd58d0dfee716ab33ea6a6b2f
Mehmet Can KAHRİMAN <98193955+mehmetckahriman@users.noreply.github.com>
2025-06-07 19:20:25 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -e
 
-# Her başlatmada daemon satırlarını sil
 sed -i '/daemon\s*off;/d;/daemon\s*on;/d' /etc/nginx/nginx.conf
 echo "NGINX CONF (daemon):"
 grep -n daemon /etc/nginx/nginx.conf || echo "YOK"
 
-php bin/console doctrine:migrations:migrate --no-interaction --env=prod
+echo "Wallabag DB migration status:"
+php bin/console doctrine:migrations:status --env=prod || true
+
+echo "Wallabag DB migrate başlatılıyor:"
+php bin/console doctrine:migrations:migrate --no-interaction --env=prod || true
+
+# İstersen force schema update de uygula:
+php bin/console doctrine:schema:update --force --env=prod || true
 
 php-fpm81 -D
 nginx -g 'daemon off;'
